94dad33088a1effc5d058765b3d08c34648348d4
khoavo1601 <dangkhoavo161@gmail.com>
2025-05-25 14:14:32 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 TEXT = "✅ Test thành công từ Railway!"
 
